58f18cb7cd1906e450cae7c6e589454fffae4368
nathanielyoon <nathanielyoon1@gmail.com>
2025-04-09 16:28:46 -0400

    reorganize

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,11 @@
 import { assertEquals } from "@std/assert";
-import { sha512 } from "./mod.ts";
+import { sha512 } from "../main.ts";
 
 Deno.test(async function nist_sha512() {
-  const nist = await Deno.readTextFile("./nist.txt");
+  const path = import.meta.url.slice(7, -8);
+  const short = await Deno.readTextFile(path + "/nist_short.txt");
+  const long = await Deno.readTextFile(path + "/nist_long.txt");
+  const nist = short + long;
   for (const step of nist.matchAll(/Len = (\d+)\s+Msg = (\S+)\s+MD = (\S+)/g)) {
     const [step1, step2] = [step[2].slice(0, +step[1] << 1), step[3]].map((Z) =>
       Uint8Array.from(Z.match(/[\da-f]{2}/g) ?? [], (Z) => parseInt(Z, 16))
